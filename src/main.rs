68890bc23d38c4f73a0420a76a13cb43bfec9a55
mandeep <mandeep@users.noreply.github.com>
2018-04-22 22:34:02 -0500

    Add preliminary line drawing algorithm

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,18 @@ extern crate image;
 use std::fs::File;
 
 
+fn line(x0: i32, y0: i32, x1: i32, y1: i32, image: &mut image::RgbImage) -> () {
+    for t in 0..10000 {
+        let t = t as f32 * 0.0001;
+        let x = x0 as f32 * (1.0 - t) + x1 as f32 * t;
+        let y = y0 as f32 * (1.0 - t) + y1 as f32 * t;
+        image.put_pixel(x as u32, y as u32, image::Rgb([255, 255, 255]));
+    }
+}
+
 fn main() {
-    let image = image::DynamicImage::new_rgb8(512, 512);
+    let mut buffer = image::ImageBuffer::new(512, 512);
+    line(0, 0, 512, 512, &mut buffer);
     let ref mut fout = File::create("output.png").unwrap();
-    image.save(fout, image::PNG).unwrap();
+    image::ImageRgb8(buffer).flipv().save(fout, image::PNG).unwrap();
 }
