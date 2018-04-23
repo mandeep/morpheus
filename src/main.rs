9ed4ae665fa35bd0e5c9ca32654f0588c3ef2c1f
mandeep <mandeep@users.noreply.github.com>
2018-04-22 19:21:48 -0500

    Use DynamicImage enum to create rgb8 in order to minimize boilerplate

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use std::fs::File;
 
 
 fn main() {
-    let buffer = image::ImageBuffer::new(512, 512);
+    let image = image::DynamicImage::new_rgb8(512, 512);
     let ref mut fout = File::create("output.png").unwrap();
-    image::ImageRgba8(buffer).save(fout, image::PNG).unwrap();
+    image.save(fout, image::PNG).unwrap();
 }
