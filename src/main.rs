e499f407bb16afebeb712968f4bd5e269c3b3859
mandeep <mandeep@users.noreply.github.com>
2018-04-28 16:55:01 -0500

    Add draw_wire_mesh function from contents in main()
    Add blank lines in draw_wire_mesh for better readability

--- src/main.rs
+++ src/main.rs
@@ -22,7 +22,6 @@ mod wavefront;
 ///
 /// line(0, 0, 511, 511, &mut buffer, image::Rgb([128, 0, 255]))
 /// ```
-///
 fn draw_line(mut x0: i32, mut y0: i32, mut x1: i32, mut y1: i32, buffer: &mut image::RgbImage, color: image::Rgb<u8>) {
     let steep = (x0 - x1).abs() < (y0 - y1).abs();
     
@@ -60,23 +59,42 @@ fn draw_line(mut x0: i32, mut y0: i32, mut x1: i32, mut y1: i32, buffer: &mut im
     }
 }
 
-fn main() {
-    let width = 1600;
-    let height = 1600;
-    let mut buffer = image::ImageBuffer::new(width, height);
-    let coordinates = wavefront::Object::new("head.obj");
+/// Draw a wire mesh on the given ImageBuffer with the coordinates from the given file
+///
+/// # Examples
+///
+/// ```
+/// let width = 512;
+/// let height = 512;
+/// let mut buffer = image::ImageBuffer::new(width, height);
+///
+/// draw_wire_mesh("coordinates.obj", &mut buffer, width, height); 
+/// ```
+fn draw_wire_mesh(filename: &str, buffer: &mut image::RgbImage, width: u32, height: u32) {
+    let coordinates = wavefront::Object::new(filename);
 
     for face in coordinates.faces {
         for i in 0..3 {
             let v0 = coordinates.vertices[(face[i] - 1) as usize];
             let v1 = coordinates.vertices[(face[(i+1) % 3] - 1) as usize];
+            
             let x0 = ((v0.x + 1.0) * width as f64 / 2.0).min(width as f64 - 1.0);
             let y0 = ((v0.y + 1.0) * height as f64 / 2.0).min(height as f64 - 1.0);
+            
             let x1 = ((v1.x + 1.0) * width as f64 / 2.0).min(width as f64 - 1.0);
             let y1 = ((v1.y + 1.0) * height as f64 / 2.0).min(height as f64 - 1.0);
-            draw_line(x0 as i32, y0 as i32, x1 as i32, y1 as i32, &mut buffer, image::Rgb([255, 255, 255]));
+            
+            draw_line(x0 as i32, y0 as i32, x1 as i32, y1 as i32, buffer, image::Rgb([255, 255, 255]));
         }
     }
+}
+
+fn main() {
+    let width = 1600;
+    let height = 1600;
+    let mut buffer = image::ImageBuffer::new(width, height);
+
+    draw_wire_mesh("head.obj", &mut buffer, width, height);    
 
     let ref mut fout = File::create("../output.png").unwrap();
     image::ImageRgb8(buffer).flipv()
