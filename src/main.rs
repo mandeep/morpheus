363fdabdbde2e58339c830dbfa5aabc4660e32bb
mandeep <mandeep@users.noreply.github.com>
2018-04-28 14:45:34 -0500

    Read lines from wavefront object file and draw wire mesh

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 extern crate image;
+extern crate nalgebra;
 
-use std::io::prelude::*;
-use std::io::BufReader;
 use std::fs::File;
 use std::mem::swap;
+
+mod wavefront;
 
 
 fn line(mut x0: i32, mut y0: i32, mut x1: i32, mut y1: i32, buffer: &mut image::RgbImage, color: image::Rgb<u8>) -> () {
@@ -45,14 +46,24 @@ fn line(mut x0: i32, mut y0: i32, mut x1: i32, mut y1: i32, buffer: &mut image::
 }
 
 fn main() {
-    let mut buffer = image::ImageBuffer::new(512, 512);
-    
-    line(0, 0, 511, 511, &mut buffer, image::Rgb([255, 255, 255]));
-    line(13, 20, 400, 200, &mut buffer, image::Rgb([255, 0, 0])); 
-    line(20, 13, 300, 100, &mut buffer, image::Rgb([0, 255, 0])); 
-    line(80, 40, 130, 200, &mut buffer, image::Rgb([0, 0, 255]));
+    let width = 1600;
+    let height = 1600;
+    let mut buffer = image::ImageBuffer::new(width, height);
+    let coordinates = wavefront::Object::new("head.obj");
 
-    let ref mut fout = File::create("output.png").unwrap();
+    for face in coordinates.faces {
+        for i in 0..3 {
+            let v0 = coordinates.vertices[(face[i] - 1) as usize];
+            let v1 = coordinates.vertices[(face[(i+1) % 3] - 1) as usize];
+            let x0 = ((v0.x + 1.0) * width as f64 / 2.0).min(width as f64 - 1.0);
+            let y0 = ((v0.y + 1.0) * height as f64 / 2.0).min(height as f64 - 1.0);
+            let x1 = ((v1.x + 1.0) * width as f64 / 2.0).min(width as f64 - 1.0);
+            let y1 = ((v1.y + 1.0) * height as f64 / 2.0).min(height as f64 - 1.0);
+            line(x0 as i32, y0 as i32, x1 as i32, y1 as i32, &mut buffer, image::Rgb([255, 255, 255]));
+        }
+    }
+
+    let ref mut fout = File::create("../output.png").unwrap();
     image::ImageRgb8(buffer).flipv()
                             .save(fout, image::PNG)
                             .unwrap();
