16c6e753479bb90b5278d8a543157804ac49ae96
mandeep <mandeep@users.noreply.github.com>
2018-04-27 22:39:56 -0500

    Change vertices type to Vec<Vector3<f64>> and add new method

--- src/wavefront.rs
+++ src/wavefront.rs
@@ -4,6 +4,15 @@ use nalgebra::core::Vector3;
 
 
 struct WaveFront {
-    vertices: Vector3<f64>,
+    vertices: Vec<Vector3<f64>>,
     faces: Vec<Vec<i32>>
+}
+
+
+impl WaveFront {
+    fn new(filename: &str) -> WaveFront {
+        let file = BufReader::new(File::open(filename).unwrap());
+        let mut vertices = Vec<Vector3<f64>> = Vec::new();
+        let mut faces = Vec<Vec<i32>> = Vec::new();
+    }
 }
