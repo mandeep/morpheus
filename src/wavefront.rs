16753ab0a96ce07f30824b9f0028a8061766675a
mandeep <mandeep@users.noreply.github.com>
2018-04-30 16:18:30 -0500

    Add doc comments for Object struct and new() method

--- src/wavefront.rs
+++ src/wavefront.rs
@@ -6,6 +6,23 @@ use std::fs::File;
 use nalgebra::core::{Vector2, Vector3};
 
 
+/// A wavefront object containing vertices and faces from a wavefront file.
+///
+/// An instance of Object will contain the geometric vertices, geometric faces,
+/// texture vertices, and texture faces from a given wavefront object file.
+///
+/// # Examples
+///
+/// Create a new Object from a given file and iterate over its vertices
+///
+/// ```
+/// let coordinates = wavefront::Object::new("file.obj");
+/// 
+/// for geometric_vertex in coordinates.geometric_vertices {
+///     // do something with the vertices
+/// }
+/// ```
+///
 pub struct Object {
     pub geometric_vertices: Vec<Vector3<f64>>,
     pub geometric_faces: Vec<Vec<i32>>,
@@ -15,6 +32,28 @@ pub struct Object {
 
 
 impl Object {
+    /// Create an instance of Object from the given filename.
+    ///
+    /// The geometric vertices are parsed from all lines that begin with
+    /// 'v' while the texture vertices are parsed from all lines that
+    /// begin with 'vt'. The faces are parsed from lines that begin
+    /// with 'f'. Faces are assumed to be in the form of a triangle triplet
+    /// such as 1100/1200/1300 1000/900/800 100/200/300 where 1100, 1000,
+    /// and 100 are the geometric faces and 1200, 900, and 200 are the
+    /// texture faces.
+    ///
+    /// # Examples
+    ///
+    /// Create a new Object from a given file and iterate over its faces.
+    ///
+    /// ```
+    /// let coordinates = wavefront::Object::new("file.obj");
+    /// 
+    /// for geometric_face in coordinates.geometric_faces {
+    ///     // do something with the faces
+    /// }
+    /// ```
+    ///
     pub fn new(filename: &str) -> Object {
         let file = BufReader::new(File::open(filename).unwrap());
         let mut geometric_vertices: Vec<Vector3<f64>> = Vec::new();
