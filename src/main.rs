8f8f6e0550c4a4d2e5ff4a1ed56c6de172ff58cc
mandeep <mandeep@users.noreply.github.com>
2018-05-03 17:30:45 -0500

    Add eye and center vectors

--- src/main.rs
+++ src/main.rs
@@ -12,11 +12,11 @@ fn main() {
     let (width, height) = (1600, 1600);
     let mut buffer = image::ImageBuffer::new(width, height);
 
-    let eye = Vector3::new(1, 1, 3);
-    let center = Vector3::new(0, 0, 0);
+    let eye = Vector3::new(1.0, 1.0, 3.0);
+    let center = Vector3::new(0.0, 0.0, 0.0);
     let light_vector = Vector3::new(0.0, 0.0, -1.0).normalize();
     
-    render::draw_triangle_mesh("../porsche.obj", &mut buffer, &light_vector);
+    render::draw_triangle_mesh("../porsche.obj", &mut buffer, &eye, &center, &light_vector);
 
     let ref mut fout = File::create("../triangle_mesh.png").unwrap();
     image::ImageRgb8(buffer).flipv()
