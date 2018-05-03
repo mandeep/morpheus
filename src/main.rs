df9e2f99693c9398ea70861f90b24222997f37cd
mandeep <mandeep@users.noreply.github.com>
2018-05-03 17:17:32 -0500

    Add eye and center vectors

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,8 @@ fn main() {
     let (width, height) = (1600, 1600);
     let mut buffer = image::ImageBuffer::new(width, height);
 
+    let eye = Vector3::new(1, 1, 3);
+    let center = Vector3::new(0, 0, 0);
     let light_vector = Vector3::new(0.0, 0.0, -1.0).normalize();
     
     render::draw_triangle_mesh("../porsche.obj", &mut buffer, &light_vector);
