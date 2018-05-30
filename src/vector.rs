157f456e4cd8e05f8b643ed2be954ca8dbd4678a
mandeep <mandeep@users.noreply.github.com>
2018-05-29 23:10:14 -0700

    Use Matrix4x1 instead of Matrix4 for vectorize and matricize functions

--- src/vector.rs
+++ src/vector.rs
@@ -2,19 +2,14 @@ extern crate image;
 extern crate nalgebra;
 
 use nalgebra::geometry::Point3;
-use nalgebra::core::Matrix4;
+use nalgebra::core:: Matrix4x1;
 
 
-pub fn vectorize(matrix: Matrix4<f64>) -> Point3<f64> {
+pub fn vectorize(matrix: Matrix4x1<f64>) -> Point3<f64> {
     Point3::new(matrix.row(0)[0] / matrix.row(3)[0], matrix.row(1)[0] / matrix.row(3)[0], matrix.row(2)[0] / matrix.row(3)[0])
 }
 
 
-pub fn matricize(point: Point3<f64>) -> Matrix4<f64> {
-    let mut matrix: Matrix4<f64> = Matrix4::identity();
-    matrix.row_mut(0)[0] = point.x;
-    matrix.row_mut(1)[0] = point.y;
-    matrix.row_mut(2)[0] = point.y;
-
-    matrix
+pub fn matricize(point: Point3<f64>) -> Matrix4x1<f64> {
+    Matrix4x1::new(point.x, point.y, point.z, 1.0)
 }
