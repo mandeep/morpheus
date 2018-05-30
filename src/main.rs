0eeba821ba70a2b0f1404b46e36588f2062dfb8c
mandeep <mandeep@users.noreply.github.com>
2018-05-29 23:45:49 -0700

    Change camera coordinates and change output filename

--- src/main.rs
+++ src/main.rs
@@ -15,14 +15,14 @@ fn main() {
     let mut buffer = image::ImageBuffer::new(width, height);
 
 
-    let eye = Vector3::new(6.0, 1.0, 10.0);
+    let eye = Vector3::new(6.0, 5.0, 30.0);
     let center = Vector3::new(0.0, 0.0, 0.0);
     let up = Vector3::new(0.0, 1.0, 0.0);
     let light_vector = Vector3::new(1.0, -1.0, 1.0).normalize();
 
     render::draw_triangle_mesh("../porsche.obj", &mut buffer, depth, &light_vector, &eye, &center, &up);
 
-    let ref mut fout = File::create("../triangle_mesh.png").unwrap();
+    let ref mut fout = File::create("../projected_triangle_mesh.png").unwrap();
     image::ImageRgb8(buffer).flipv()
                             .save(fout, image::PNG)
                             .unwrap();
