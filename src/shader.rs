253f4946e3b53ef287fd4c12ca0d9f92ccc3548a
mandeep <mandeep@users.noreply.github.com>
2018-06-01 21:17:23 -0700

    Add vertex and fragment shaders

--- src/shader.rs
+++ src/shader.rs
@@ -1,20 +1,29 @@
 extern crate nalgebra;
 extern crate image;
 
-use nalgebra::core::Vector3;
+use nalgebra::core::{Matrix4, Matrix4x1, Vector3};
+
+mod vector;
+mod wavefront;
 
 
 struct GoraudShader {
-    varying_intensity: f64,
+    pub varying_intensity: Vector3<f64>,
 }
 
 
 impl GoraudShader {
-    fn vertex(face_index: usize, vertex_index: usize) {
+    fn vertex(coordinates: &wavefront::Object, view_port: &Matrix4<f64>, projection: &Matrix4<f64>, model_view: &Matrix4<f64>, light_vector: &Vector3<f64>, face_index: usize, vertex_index: usize) -> Matrix4x1<f64> {
+        varying_intensity[vertex_index] = 0.0.max(coordinates.normal_faces[face_index][vertex_index].normalize().cross(&light_vector));
+        let gl_vertex: Matrix4x1<f64> = vector::matricize(coordinates.geometric_vertices[face_index][vertex_index]);
 
+        view_port * projection * model_view * gl_vertex
     }
 
-    fn fragment(pixel: Vector3<f64>, color: image::Rgb<u8>) {
+    fn fragment(pixel: Vector3<f64>, color: &mut image::Rgb<u8>) -> bool {
+        let intensity: f64 = varying_intensity.cross(&pixel);
+        color = image::Rgb([(255.0 * intensity) as u8, (255.0 * intensity) as u8, (255.0 * intensity) as u8]);
 
+        false
     }
 }
