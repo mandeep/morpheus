aa0fa4530a3961acba9153d426035ecebf7c2bbb
mandeep <mandeep@users.noreply.github.com>
2018-06-12 00:02:48 -0700

    Swap vertex index and face index in parameters

--- src/shader.rs
+++ src/shader.rs
@@ -14,12 +14,15 @@ pub struct GouraudShader {
 
 impl GouraudShader {
     pub fn vertex(&mut self, coordinates: &wavefront::Object, view_port: &Matrix4<f64>, projection: &Matrix4<f64>, model_view: &Matrix4<f64>,
-                  light_vector: &Vector3<f64>, vertex_index: usize, face_index: usize) -> Vector4<f64> {
+                  light_vector: &Vector3<f64>, face_index: usize, vertex_index: usize) -> Vector4<f64> {
 
-        self.varying_intensity[vertex_index] = 0.0f64.max(coordinates.normal_faces[face_index].map(|n| n as f64)
-                                                                                              .normalize()
-                                                                                              .dot(&light_vector));
-        let gl_vertex: Vector4<f64> = vector::vectorize_to_4d(coordinates.geometric_vertices[face_index]);
+        let normal_index = coordinates.normal_faces[face_index][vertex_index] as usize;
+        let geometric_index = coordinates.geometric_faces[face_index][vertex_index] as usize;
+
+        self.varying_intensity[vertex_index] = 0.0f64.max(coordinates.normal_vertices[normal_index].map(|n| n as f64)
+                                                                                                   .normalize()
+                                                                                                   .dot(&light_vector));
+        let gl_vertex: Vector4<f64> = vector::vectorize_to_4d(coordinates.geometric_vertices[geometric_index]);
 
         view_port * projection * model_view * gl_vertex
     }
